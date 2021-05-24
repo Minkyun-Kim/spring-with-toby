1c0c0f24e462a43c9923992b155dd3735dd48438
brandon <mkk0618@naver.com>
2021-05-24 21:54:31 +0900

    1.5 스프링의 IoC
    DaoFactory로 구현했던 IoC를 스프링 프레임워크의 ApplicationContext를 이용해서 구현한다.
    ApplicationContext는 BeanFactory를 상속하는 인터페이스로 통상적으로 빈 팩토리, 컨테이너, 스프링이라고 부르기도 한다. ApplicationContext가 빈을 관리할 수 있도록 설정 정보를 포함하는 클래스에 @Configuration을 달아준다. 해당 클래스 내에 빈을 생성하는 메소드는 @Bean을 달아준다. ApplicationContext는 Bean 목록을 유지하며 getBean() 메소드로 빈을 요청할때 Bean 목록을 살펴 빈 팩토리에 객체를 요구하여 반환해준다. getBean()의 첫번째 파라미터로 Bean의 이름을 넘겨주는데, Bean의 이름은 메소드 이름과 같다. 두번째 파라미터는 해당 빈의 클래스 타입인데, 원래 getBean()은 Object를 리턴하기 때문에 캐스팅 해줘야 하는 번거로움이 있어 UserDao.class 처럼 타입을 넘겨 깔끔한 코드를 만들어준다.

--- src/main/java/springbook/user/domain/DaoFactory.java
+++ src/main/java/springbook/user/domain/DaoFactory.java
@@ -1,11 +1,16 @@
 package springbook.user.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration//빈 팩토리용 오브젝트 설정 클래스라고 지칭
 public class DaoFactory {
 
+    @Bean//오브젝트를 만들어 주는 곳에 붙임.
     public UserDao userDao(){
         return new UserDao(getConnectionMaker());
     }
-
+    @Bean
     public ConnectionMaker getConnectionMaker(){
         return new DConnectionMaker();
     }
